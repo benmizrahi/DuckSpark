911eb1dd156be33f2d95d80210cff1f63783d7bb
Ben Mizrahi <benm@item-soft.com>
2023-03-12 09:51:58 +0000

    added gin web GRPC

--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,17 @@
 package worker
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Worker struct {
+	MaxParallel int
+	Master      string
+	Port        int
+}
+
+func NewWorker() *Worker {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {})
+	return &Worker{}
+}
