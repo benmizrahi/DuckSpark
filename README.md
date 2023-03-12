be3491034bf0b08595875189d4ef0e1614da62fd
Ben Mizrahi <benm@item-soft.com>
2023-03-12 22:09:24 +0200

    start workers

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-A Golnag distributed data processing engine - inspired by OSS  
+A Golnag distributed data processing engine - inspired by Apache Spark  
+
+
+
+###
+``protoc --go_out=. .proto/*.proto```
--- worker/worker.go
+++ worker/worker.go
@@ -1,17 +1,44 @@
 package worker
 
 import (
+	"net/http"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/benmizrahi/godistodist/proto"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type Worker struct {
 	MaxParallel int
 	Master      string
 	Port        int
+	Http        *gin.Engine
 }
 
-func NewWorker() *Worker {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {})
-	return &Worker{}
+func NewWorker(master string) *Worker {
+	return &Worker{
+		Port:        9999,
+		MaxParallel: 10,
+		Master:      `http://` + master + `:9991/api`,
+		Http:        gin.Default(),
+	}
+}
+
+func (w *Worker) Init() {
+	w.Http.GET("/api/v1/health", func(c *gin.Context) {
+		res := &proto.HCRes{
+			Uuid: uuid.New().String(),
+			Time: timestamppb.Now(),
+		}
+		c.ProtoBuf(http.StatusOK, res)
+	})
+}
+
+func (w *Worker) Start() {
+	w.Http.Run(":" + string(rune(w.Port)))
+}
+
+func (w *Worker) Stop() {
 }
